4e6814228ce5a662721cf8f1c1037153151cad30
AbdulHameedi <sanilinity507@gmail.com>
2024-10-08 00:11:25 +0100

    feedback route and half design

---
+++ src/pages/Feedback.tsx
@@ -0,0 +1,20 @@
+export const Feedback = () => {
+  return (
+    <section className="flex items-center justify-center h-screen bg-[#161616]">
+    <div className="modal text-[#D7D7D7] font-semibold text-xl w-[920px] rounded-[24px] bg-[#1C1C1C] p-6">
+      <div className="header ">
+        <div className="flex justify-between items-center leading-6">
+            <h2 className="text-xl">Feedback form</h2>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#5A5A5A" d="m12 13.4l-2.9 2.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l2.9-2.9l-2.9-2.875q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l2.9 2.9l2.875-2.9q.275-.275.7-.275t.7.275q.3.3.3.713t-.3.687L13.375 12l2.9 2.9q.275.275.275.7t-.275.7q-.3.3-.712.3t-.688-.3z"/></svg>
+        </div>
+            <p className="text-[#BDBDBD] text-xs">Need any feature improved on or included? Send us an email</p>
+      </div> {/* HEADER */}
+
+      <div>
+        
+      </div>
+    </div> {/* MODAL */}
+    </section>
+  )
+}
+

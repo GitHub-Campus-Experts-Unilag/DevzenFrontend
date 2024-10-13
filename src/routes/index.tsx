50e1c4240f97f08c72bac1d7e0eb442258ae97a0
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2024-10-13 22:04:46 +0100

    Implemented desktop view of the landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
 import SignIn from "../pages/auth/SignIn";
 
 export const router = createBrowserRouter([ 
+    {
+        path: "/",
+        element: <Home />,
+    },
     {
         path: "/login",
         element: <SignIn />,

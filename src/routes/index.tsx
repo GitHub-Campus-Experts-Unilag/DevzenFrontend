5993959e12b1e6887833ba4b0f5bb66094aab610
Bobate Olusegun <bobatesegun3gmail.com>
2024-10-20 14:29:56 +0100

    added-routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignIn from "../pages/auth/SignIn";
 import { Feedback } from "../pages/Feedback";
+import NumberBaseConverter from '../components/converters/NumberBaseConverter'
 
 export const router = createBrowserRouter([ 
     {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
     {
         path: "/feedback",
         element: <Feedback />
+    },
+    {
+        path: "/numberBaseConverter",
+        element: <NumberBaseConverter/>
     }
 ])
